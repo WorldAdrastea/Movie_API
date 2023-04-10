bb5f5e599e4eb9c6bd1d2e5b697178afacf1cbd2
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-04-10 02:01:19 +0100

    Ex 2.5 troubleshoot ver
    If applied will:
    - Add endpoints to search by title and genre
    - Update the movies array to only have strings
    - Added a users array

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//requires modules
 const express = require('express'),
     morgan = require('morgan');
 
@@ -6,20 +7,46 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-let topMovies = [
+
+//users
+let users = [
     {
-        title: 'Puss in Boots: The Last Wish',
-        year: '(2022)'
+        id: 1,
+        name: 'Nana',
+        favMovie: []
     },
     {
-        title: 'Morbius',
-        year: '(2022)'
+        id: 2,
+        name: 'Pat',
+        favMovie: ['Morbius']
+    },
+];
+//list of top movies
+let movies = [
+    {
+        'title': 'Puss in Boots: The Last Wish',
+        'year': '(2022)',
+        'description': 'The third Puss in Boots movie of the series',
+        'genre': 'Animation',
+        'director': ''
     },
     {
-        title: 'Bee Movie',
-        year: '(2007)'
+        'title': 'Morbius',
+        'year': '(2022)',
+        'description': 'Morbius',
+        'genre': 'Supernatural',
+        'director': ''
+    },
+    {
+        'title': 'Bee Movie',
+        'year': '(2007)',
+        'description': 'Movie about bees',
+        'genre': 'Animation',
+        'director': ''
     }
 ];
+
+//READ
 
 app.get('/', (req,res) => {
     let responseText = 'Welcome!';
@@ -30,10 +57,32 @@ app.get('/', (req,res) => {
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname});
 });
-
+//Gets list of movies
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.status(200).json(movies);
 });
+//Gets movies by title
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = movies.find(movie => movie.title === title);
+
+    if (movie) {
+        res.status(200).json(movie)
+    } else {
+        res.status(400).send('no such movie')
+    }
+});
+//Gets movies by genre
+app.get('/movies/genre/:genreName', (req,res) => {
+    const {genreName} = req.params;
+    const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('no such genre')
+    }
+})
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
