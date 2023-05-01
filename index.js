e468d7b7c6a83bc486fd2cf2393db30c0f05ecb3
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-05-01 16:57:56 +0100

    myFlix Application Heroku Port v1.0.0
    If applied, commit will:
    - Add cors, express validator and bycrypt
    - update script with start: node index.js
    - add validation logic
    - change port logic

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require("express"),
   mongoose = require("mongoose"),
   Models = require("./models.js");
 
+const {check, validationResult} = require('express-validator');
+
 const Movies = Models.Movie;
 const Users = Models.User;
 
@@ -18,7 +20,24 @@ mongoose.connect("mongodb://127.0.0.1:27017/MoviesAPI", {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const cors = require('cors');
+let allowedOrigins = [
+  'http://localhost:8080',
+  'http://testsite.com'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null,true);
+  }
+}));
 let auth = require("./auth.js")(app);
+
 const passport = require("passport");
 require("./passport.js");
 
@@ -142,7 +161,21 @@ app.get(
 );
 
 //CREATE
-app.post("/users", (req, res) => {
+app.post("/users", 
+//Validation logic
+[
+  check('Username', 'Username is required').isLength({min:4}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], 
+(req, res) => {
+  //Check the Validation Object for errors
+  let errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).json({errors: errors. array() });
+  }
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -150,7 +183,7 @@ app.post("/users", (req, res) => {
       } else {
         Users.create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         })
@@ -173,6 +206,12 @@ app.post("/users", (req, res) => {
 app.put(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
+  [
+    check('Username', 'Username is required').isLength({min:4}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ],
   (req, res) => {
     Users.findOneAndUpdate(
       { Username: req.params.Username },
@@ -270,11 +309,14 @@ app.delete(
   }
 );
 
+//Error Handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
-app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
+//Port
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+  console.log('Listening on Port ' + port);
 });
